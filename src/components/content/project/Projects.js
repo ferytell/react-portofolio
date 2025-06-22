7d7dfd0d5bee37c664ad49d929f7d5692ecb8472
ferytell <darkstar2709@gmail.com>
2025-06-22 12:36:24 +0700

    testing agan

--- src/components/content/project/Projects.js
+++ src/components/content/project/Projects.js
@@ -1,6 +1,5 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-//import projImg1 from "../../../assets/img/project-img.png";
 import imageNotAvailable from "../../../assets/img/notavailable.png";
 import shareStory from "../../../assets/img/ScreenshotShareStory.png";
 import colorSharp2 from "../../../assets/img/color-sharp-1.png";
